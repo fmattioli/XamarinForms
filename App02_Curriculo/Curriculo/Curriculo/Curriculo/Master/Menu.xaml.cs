93be64a8535292da8eb9e83d6077dc42df94cbad
Felipe <felipe-mattioli98@hotmail.com>
2020-05-07 21:58:08 -0300

    first commit

---
+++ App02_Curriculo/Curriculo/Curriculo/Curriculo/Master/Menu.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Curriculo.Master
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : MasterDetailPage
+	{
+		public Menu ()
+		{
+			InitializeComponent ();
+		}
+
+        private  void GoCarreira(object sender, EventArgs args)
+        {
+            Detail= new Pages.Perfil();
+
+        }
+        private void GoEventos(object sender, EventArgs args)
+        {
+            Detail = new Pages.Sobre();
+        }
+
+        private void GoEducacional(object sender, EventArgs args)
+        {
+            Detail = new Pages.Educacional();
+        }
+        private void GoSkills(object sender, EventArgs args)
+        {
+            Detail = new Pages.Skills();
+        }
+    }
+}
